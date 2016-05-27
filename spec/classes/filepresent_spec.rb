b4f78ad3f6b1a55713ddfb87b014b8bb52360a9b
IswaryaSekar <ishuripi@gmail.com>
2016-05-27 13:52:06 +0000

    unit testcases

--- spec/classes/filepresent_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-#require_relative '../manifests/sample.rb'
-describe 'contains install_logging' do
-it { File.exist?('Mymodule/install_logging/manifests/init.pp') }
-end
-at_exit { RSpec::Puppet::Coverage.report! }
---
+++ spec/classes/gclogging_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe 'gclogging' do
+it { should contain_class('gclogging') }
+
+end
